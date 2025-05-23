7edde4e649e7b1f0b17c6460e99b2af28567b9a0
kimura <dnanf12345@gmail.com>
2025-05-23 14:08:24 +0900

    feat: repository

---
+++ apps/user/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@repo/ui/components/ui/button';
+
+export default function page() {
+  return (
+    <div>
+      <Button>되냐?</Button>
+    </div>
+  );
+}
---
+++ apps/user/app/(main)/search/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+  // 검색어 입력 후 query string(`?q=...`)으로 검색 결과 표시
+  //결과는 실시간 fetch로 불러오며, 상태 관리는 zustand 사용
+  return <div>page</div>;
+}
