de424fccb71431bb5c692c50e6e13442147f9033
KIM DONG HYUN <zofhfhguddk@naver.com>
2025-06-10 00:04:53 +0900

    feat: nextauth api 연결 그리고 미들웨어 연결

---
+++ apps/user/middleware.ts
@@ -0,0 +1,65 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { routes } from './config/routes';
+
+const FALLBACK_URL = '/main';
+
+const withAuth = async (req: NextRequest, token: boolean) => {
+  const url = req.nextUrl.clone();
+  const { pathname } = req.nextUrl;
+
+  if (!token) {
+    url.pathname = routes.signIn;
+    url.search = `callbackUrl=${pathname}`;
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+};
+
+const withOutAuth = async (
+  req: NextRequest,
+  token: boolean,
+  to: string | null
+) => {
+  const url = req.nextUrl.clone();
+
+  if (token) {
+    url.pathname = to ?? FALLBACK_URL;
+    url.search = '';
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+};
+
+const PUBLIC_ROUTES = ['/', routes.signIn];
+
+export default async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  const accessToken = token?.accessToken;
+  const { pathname, searchParams } = request.nextUrl;
+  const callbackUrl = searchParams.get('callbackUrl');
+
+  const isPublic = PUBLIC_ROUTES.includes(pathname);
+
+  if (pathname === routes.signIn) {
+    return withOutAuth(request, !!accessToken, callbackUrl);
+  }
+  if (isPublic) {
+    return NextResponse.next();
+  }
+
+  return withAuth(request, !!accessToken);
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon\\.ico|fonts|images|background).*)',
+  ],
+};
