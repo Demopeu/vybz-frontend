c2a80f47798dd2a9d71b21c4421bdd854c849819
kimura <dnanf12345@gmail.com>
2025-07-08 11:27:02 +0900

    fix: 디자인 수정

--- apps/user/app/(sub)/chat/[buskerId]/page.tsx
+++ apps/user/app/(sub)/chat/[buskerId]/page.tsx
@@ -1,12 +1,11 @@
 import ChatHeader from '@/components/common/layouts/header/ChatHeader';
-import DmBar from '@/components/common/form/DmBar';
 import { UseChat } from '@/context/ChatContext';
 import { UseModal } from '@/context/ModalContext';
 import { getServerSession } from 'next-auth';
 import { options } from '@/app/api/auth/[...nextauth]/options';
 import ChatMessageList from '@/components/chat/rooms/ChatMessageList';
-import { unstable_ViewTransition as ViewTransition } from 'react';
 import { BuskerInfoReadService } from '@/services/info-services/BuskerInfoReadService';
+import ChatDmBar from '@/components/chat/rooms/ChatDmBar';
 
 interface PageProps {
   params: Promise<{
@@ -26,24 +25,22 @@ export default async function page({ params, searchParams }: PageProps) {
 
   const buskerInfo = await BuskerInfoReadService(buskerId);
   return (
-    <div className="relative min-w-80 h-screen text-white">
+    <div className="relative min-w-80 h-screen text-white bg-gradient-to-b from-black via-gray-900 to-black">
       <ChatHeader buskerUuid={buskerId} buskerInfo={buskerInfo} />
-      <ViewTransition name="blue-label">
-        <main className="absolute inset-x-0 bottom-0 top-29 flex flex-col px-4 bg-blue-400 rounded-tr-4xl border-t-2 border-indigo-900">
+      <main className="absolute inset-x-0 bottom-0 top-20 flex flex-col px-4 bg-transparent">
+        <UseModal>
+          <ChatMessageList
+            chatRoomId={chatRoomId}
+            userUuid={userUuid}
+            buskerUuid={buskerId}
+          />
+        </UseModal>
+        <UseChat>
           <UseModal>
-            <ChatMessageList
-              chatRoomId={chatRoomId}
-              userUuid={userUuid}
-              buskerUuid={buskerId}
-            />
+            <ChatDmBar userUuid={userUuid} />
           </UseModal>
-          <UseChat>
-            <UseModal>
-              <DmBar userUuid={userUuid} />
-            </UseModal>
-          </UseChat>
-        </main>
-      </ViewTransition>
+        </UseChat>
+      </main>
     </div>
   );
 }
